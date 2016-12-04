aa00ec04fe90099411cbfa6063f0b3e92da6d0a6
Raymond Seger <raymondseger@live.com>
2016-12-04 11:35:12 +0700

    data from ajax

--- main.js
+++ main.js
@@ -2,14 +2,7 @@ $(document).ready(function() {
 
     var table = $('#example').DataTable({
 
-        "aoColumns": [
-            null,
-            null,
-            { "orderSequence": [ "asc" ] },
-            { "orderSequence": [ "desc", "asc", "asc" ] },
-            { "orderSequence": [ "desc" ] },
-            null
-        ]
+        "ajax": 'exampledata.json'
 
     });
 
