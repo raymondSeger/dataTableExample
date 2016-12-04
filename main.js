705065e7cb460b95564c14dd1f75d2e844460816
Raymond Seger <raymondseger@live.com>
2016-12-04 10:24:11 +0700

    It is possible to easily customise the options shown in the length menu (by default at the top left of the table) using the lengthMenu initi
    It is possible to easily customise the options shown in the length menu
    (by default at the top left of the table) using the lengthMenu
    initialisation option.

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 $(document).ready(function() {
     var table = $('#example').DataTable({
 
-        "columnDefs": [
-            {
-                // The `data` parameter refers to the data for the cell (defined by the
-                // `data` option, which defaults to the column being worked with, in
-                // this case `data: 0`.
-                "render": function ( data, type, row ) {
-                    return data +' ('+ row[3]+')';
-                },
-                "targets": 0
-            }
+        "lengthMenu": [
+            [25, 50, -1],
+            [25, 50, "All"]
         ]
 
     });
