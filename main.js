edc7f2e63ce468cad6104d45cbd97d8a13340cb7
Raymond Seger <raymondseger@live.com>
2016-12-04 10:43:50 +0700

    DataTables inserts DOM elements around the table to control DataTables features
    DataTables inserts DOM elements around the table to control DataTables
    features

--- main.js
+++ main.js
@@ -2,18 +2,10 @@ $(document).ready(function() {
 
     var table = $('#example').DataTable({
 
-        "footerCallback": function ( row, data, start, end, display ) {
-            console.log(row);
-            console.log(data);
-            console.log(start);
-            console.log(end);
-            console.log(display);
-        }
+        "dom": '<"toolbar">frtip'
 
     });
 
-    table.on( 'order.dt',  function () { console.log( 'Order' ); } );
-    table.on( 'search.dt', function () { console.log( 'Search' ); } );
-    table.on( 'page.dt',   function () { console.log( 'Page' ); } );
+    $("div.toolbar").html('<b>Custom tool bar! Text/images etc.</b>');
 
 } );
