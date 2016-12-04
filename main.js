415b53089b07cd98628032487a668aa3f435b343
Raymond Seger <raymondseger@live.com>
2016-12-04 10:37:19 +0700

    callback function can be used to format a particular row at draw time. For each row that is generated for display
    callback function can be used to format a particular row at draw time.
    For each row that is generated for display

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
 
-    $.extend( true, $.fn.dataTable.defaults, {
-        "searching": false,
-        "ordering": false
-    } );
-
     var table = $('#example').DataTable({
 
+        "createdRow": function ( row, data, index ) {
+            console.log(row);
+            console.log(data);
+            console.log(index);
+            $('td', row).eq(5).addClass('highlight');
+        }
+
     });
-
-
-
 
     table.on( 'order.dt',  function () { console.log( 'Order' ); } );
     table.on( 'search.dt', function () { console.log( 'Search' ); } );
