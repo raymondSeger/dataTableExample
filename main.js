81b18bfde8f0ccf0e0aaa18addaec8977904c49d
Raymond Seger <raymondseger@live.com>
2016-12-04 10:22:09 +0700

    Each column has an optional rendering control called columns.render which can be used to process the content of each cell before the data is
    Each column has an optional rendering control called columns.render
    which can be used to process the content of each cell before the data is
    used

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
     var table = $('#example').DataTable({
 
+        "columnDefs": [
+            {
+                // The `data` parameter refers to the data for the cell (defined by the
+                // `data` option, which defaults to the column being worked with, in
+                // this case `data: 0`.
+                "render": function ( data, type, row ) {
+                    return data +' ('+ row[3]+')';
+                },
+                "targets": 0
+            }
+        ]
+
     });
 
     table.on( 'order.dt',  function () { console.log( 'Order' ); } );
