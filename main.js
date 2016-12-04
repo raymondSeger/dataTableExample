ae95294bc4af77087e8e3fffdf03167c72257be5
Raymond Seger <raymondseger@live.com>
2016-12-04 09:28:59 +0700

    use state-save, which will remember the user's choice using localstorage
    use state-save, which will remember the user's choice using localstorage

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 $(document).ready(function() {
     $('#example').DataTable({
-        "columnDefs": [
-            {
-                "targets": [ 0 ],
-                "visible": false,
-                "searchable": false
-            },
-            {
-                "targets": [ 1 ],
-                "visible": false
-            }
-        ]
+        stateSave: true
     });
 } );
